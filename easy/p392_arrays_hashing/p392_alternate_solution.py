b8827374075bccd2625ea49c8f047cd8292dc0a5
Asif-GD <asif.coach@gmail.com>
2023-03-04 05:28:55 +0530

    added neetcode's solution

---
+++ easy/p392_arrays_hashing/p392_alternate_solution.py
@@ -0,0 +1,11 @@
+# neetcode's solution
+
+def is_subsequence(s: str, t: str) -> bool:
+    i, j = 0, 0
+
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+
+    return True if i == len(s) else False
