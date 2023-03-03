0dd880c4885517bfb8835bce99aa867b7c47dccc
Asif-GD <asif.coach@gmail.com>
2023-03-03 22:11:26 +0530

    a better python solution to problem 1299 on leet code - fails at test case 67 - time limit exceeded

---
+++ easy/p1299_arrays_hashing/p1299_alternate_solution.py
@@ -0,0 +1,19 @@
+def replace_elements(self, arr: list[int]) -> list[int]:
+    output_list = list()
+    if len(arr) == 1:
+        output_list.append(-1)
+        return output_list
+    else:
+        current_index = 0
+        for item in arr:
+            if current_index == len(arr) - 1:
+                output_list.append(-1)
+                return output_list
+            else:
+                next_index = current_index + 1
+                highest_to_the_right = max(arr[next_index:len(arr)])
+                output_list.append(highest_to_the_right)
+                current_index += 1
+
+
+print(replace_elements(list, [17, 18, 5, 4, 6, 1]))
