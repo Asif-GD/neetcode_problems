eb2906a76b9bccc7e79cd3c9483c9c0c1a30b374
Asif-GD <asif.coach@gmail.com>
2023-03-03 22:30:26 +0530

    python solution to problem 1299 in leet code - passed all test cases

---
+++ easy/p1299_arrays_hashing/p1299_optimal_solution.py
@@ -0,0 +1,14 @@
+# initial max = -1
+# we iterate through the array in reverse
+# compare initial max to the previous element in the array
+
+def replace_elements(self, arr: list[int]) -> list[int]:
+    current_max = -1
+    for index in range(len(arr) - 1, -1, -1):
+        new_max = max(current_max, arr[index])
+        arr[index] = current_max
+        current_max = new_max
+    return arr
+
+
+print(replace_elements(list, [17, 18, 5, 4, 6, 1]))
