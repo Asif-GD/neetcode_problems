8efb17a8b060f61992bac5d62e3228b64953f58b
Asif-GD <asif.coach@gmail.com>
2023-03-03 22:10:18 +0530

    python solution to problem 1299 on leet code - fails at test case 11 - time limit exceeded

---
+++ easy/p1299_arrays_hashing/p1299_replace_elements.py
@@ -0,0 +1,16 @@
+def replace_elements(self, arr: list[int]) -> list[int]:
+    output_list = list()
+    if len(arr) == 1:
+        output_list.append(-1)
+        return output_list
+    else:
+        for index in range(0, len(arr)):
+            temp_arr = arr.copy()
+            if len(temp_arr) == 1:
+                output_list.append(-1)
+                return output_list
+            else:
+                temp_arr.pop(0)
+                temp_arr.sort(reverse=True)
+                output_list.append(temp_arr[0])
+                arr.pop(0)
