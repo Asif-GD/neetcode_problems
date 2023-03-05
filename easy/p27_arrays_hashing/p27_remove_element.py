1157be98bd8128b0c30138106eb3869929eefb42
Asif-GD <asif.coach@gmail.com>
2023-03-05 20:10:58 +0530

    made a bit more time efficient by removing the - nums.append("_") as it is unnecessary

--- easy/p27_arrays_hashing/p27_remove_element.py
+++ easy/p27_arrays_hashing/p27_remove_element.py
@@ -1,10 +1,11 @@
 def remove_element(nums: list[int], val: int) -> int:
     number_of_val_in_nums = nums.count(val)
+    output = len(nums) - number_of_val_in_nums
     if number_of_val_in_nums != 0:
         for loop_number in range(0, number_of_val_in_nums):
             nums.remove(val)
-            nums.append("_")
-    return len(nums) - number_of_val_in_nums
+            # nums.append("_") -> not necessary
+    return output
 
 
 print(remove_element([0, 1, 2, 2, 3, 0, 4, 2], 2))
