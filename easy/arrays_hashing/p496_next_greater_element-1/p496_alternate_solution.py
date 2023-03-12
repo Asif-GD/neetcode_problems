a030fcc0e0fddf095af6f17a874fdfaae67b3532
Asif-GD <asif.coach@gmail.com>
2023-03-12 17:41:32 +0530

    an alternate solution
    - 10ms faster xD

--- easy/arrays_hashing/p496_next_greater_element-1/p496_alternate_solution.py
+++ easy/arrays_hashing/p496_next_greater_element-1/p496_alternate_solution.py
@@ -3,7 +3,7 @@ def next_greater_element(nums1: list[int], nums2: list[int]) -> list[int]:
 
     for index_1, number1 in enumerate(nums1):
         number1_position = nums2.index(number1)
-        for index_2 in range(number1_position, len(nums2)):
+        for index_2 in range(number1_position + 1, len(nums2)):
             if nums2[index_2] > number1:
                 output[index_1] = nums2[index_2]
                 break
