7a26423f3d6ea37fbd406c00a011b1ddb4e271f1
Asif-GD <asif.coach@gmail.com>
2023-03-13 14:10:42 +0530

    my python solution to problem 724 on leetcode.
    - it worked on the first try xD
    - took around 8 secs
    - but memory efficient

---
+++ easy/arrays_hashing/p724_find_pivot_index/p724_find_pivot_index.py
@@ -0,0 +1,23 @@
+"""
+Algorithm:
+start from the left -> because they have asked us to find only the left-most pivot index
+"""
+
+
+def pivot_index(nums: list[int]) -> int:
+    nums = [0] + nums + [0]
+    output = -1
+    # print(f"nums = {nums}")
+    for index in range(1, len(nums) - 1):
+        # print(f"index = {index}")
+        sum_left = sum(nums[:index])
+        sum_right = sum(nums[index + 1:])
+        # print(f"sum_left <-> sum_right = {sum_left} <-> {sum_right}")
+        if sum_left == sum_right:
+            output = index - 1  # to counter the addition of zero at start
+            break
+
+    return output
+
+
+print(pivot_index([1, 7, 3, 6, 5, 6]))
