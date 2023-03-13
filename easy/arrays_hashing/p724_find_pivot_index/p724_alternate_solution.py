6729d9d53409e20736a86c6788dbd9024625127a
Asif-GD <asif.coach@gmail.com>
2023-03-13 14:41:34 +0530

    same algorithm but a different implement, and it's way faster

---
+++ easy/arrays_hashing/p724_find_pivot_index/p724_alternate_solution.py
@@ -0,0 +1,17 @@
+"""
+the idea remains the same, but the implementation is different
+"""
+
+
+def pivot_index(nums: list[int]) -> int:
+    sum_left, sum_right = 0, sum(nums)
+
+    for index, number in enumerate(nums):
+        sum_right -= number
+        if sum_left == sum_right:
+            return index
+        sum_left += number
+    return -1
+
+
+print(pivot_index([1, 7, 3, 6, 5, 6]))
