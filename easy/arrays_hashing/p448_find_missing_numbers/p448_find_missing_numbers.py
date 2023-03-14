9e070d2bcfe301f6c8fe921e0b1dda989326ae4f
Asif-GD <asif.coach@gmail.com>
2023-03-14 13:03:54 +0530

    my python solution to problem 448 on leetcode.
    - time efficient

---
+++ easy/arrays_hashing/p448_find_missing_numbers/p448_find_missing_numbers.py
@@ -0,0 +1,15 @@
+def find_missing_numbers(nums: list[int]) -> list[int]:
+    output = list()
+    nums_set = set(nums)
+    for number in range(1, len(nums) + 1):  # to loop from 1 to n
+        if number not in nums_set:
+            output.append(number)
+        else:
+            nums_set.remove(number)
+
+    return output
+
+
+print(find_missing_numbers([4, 3, 2, 7, 8, 2, 3, 1]))
+print(find_missing_numbers([1, 1]))
+print(find_missing_numbers([1, 3, 5, 7]))
