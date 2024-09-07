e2a986184c0a8aa95f832eda96e1be93569ea011
Asif-GD <asif.coach@gmail.com>
2024-09-07 11:12:44 +0530

    used the solution to test

--- easy/arrays_hashing/p242_valid_anagram/p242_valid_anagram_002.py
+++ easy/arrays_hashing/p242_valid_anagram/p242_valid_anagram_002.py
@@ -1,17 +1,20 @@
-class Solution:
-    def is_anagram(self, s: str, t: str) -> bool:
-        # if the length of the two strings are different, they aren't anagrams
-        if len(s) != len(t):
-            return False
-        else:
-            # problem: this sorting returns a list, which takes up extra memory.
-            # sorted_s = sorted(s)
-            # sorted_t = sorted(t)
-
-            # solution: apply string join.
-            s = ''.join(sorted(s))
-            t = ''.join(sorted(t))
-            if s == t:
-                return True
-
+def is_anagram(s: str, t: str) -> bool:
+    # if the length of the two strings are different, they aren't anagrams
+    if len(s) != len(t):
         return False
+    else:
+        # problem: this sorting returns a list, which takes up extra memory.
+        # sorted_s = sorted(s)
+        # sorted_t = sorted(t)
+
+        # solution: apply string join.
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        if s == t:
+            return True
+
+    return False
+
+
+print(is_anagram("anagram", "nagaram"))
+print(is_anagram("car", "rat"))
