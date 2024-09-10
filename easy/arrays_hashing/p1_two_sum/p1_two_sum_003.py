ba67331d97bd00c132ddfcb022c7e66a1ab89627
Asif-GD <asif.coach@gmail.com>
2024-09-10 12:45:17 +0530

    set explicit typing for variables - solution - hash_map

--- easy/arrays_hashing/p1_two_sum/p1_two_sum_003.py
+++ easy/arrays_hashing/p1_two_sum/p1_two_sum_003.py
@@ -1,11 +1,14 @@
+from typing import Dict
+
+
 def two_sum(nums: list[int], target: int) -> list[int]:
-    solution = list()
+    solution: list[int] = list()
     # because there is a guaranteed solution
-    hash_map = dict()
+    hash_map: dict[int, int] = dict()
 
     for index in range(len(nums)):
         remainder = target - nums[index]
-        if remainder in hash_map: # the current index will always be greater than the index of remainder in hashmap
+        if remainder in hash_map:  # the current index will always be greater than the index of remainder in hashmap
             solution.append(hash_map[remainder])
             solution.append(index)
         else:
