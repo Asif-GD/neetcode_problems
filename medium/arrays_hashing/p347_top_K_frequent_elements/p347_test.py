bc08a6fba4bbee498814e73727a1b8ef4dca3001
Asif-GD <asif.coach@gmail.com>
2024-10-03 12:22:41 +0530

    - understand looping through dict()

--- medium/arrays_hashing/p347_top_K_frequent_elements/p347_test.py
+++ medium/arrays_hashing/p347_top_K_frequent_elements/p347_test.py
@@ -1,15 +1,33 @@
 from collections import Counter
 
-sample_list: list[int] = [1, 1, 1, 2, 2, 3, 4, 4, 4, ]
-counts = Counter(sample_list)
-print(counts)
+# sample_list: list[int] = [1, 1, 1, 2, 2, 3, 4, 4, 4, ]
+# counts = Counter(sample_list)
+# print(counts)
+#
+# most_common = list()
+# for key, value in counts.items():
+#     print(key)
+#     print(value)
+#     if value >= 2:
+#         most_common.append(key)
+#
+# # print(counts.most_common())
+# print(most_common)
 
-most_common = list()
-for key, value in counts.items():
-    print(key)
-    print(value)
-    if value >= 2:
-        most_common.append(key)
+sample_dict = {
+    1: 3,
+    2: 2,
+    3: 1,
+}
 
-# print(counts.most_common())
-print(most_common)
+for item in sample_dict:
+    print(f"default: {item}")
+
+for item in sample_dict.items():
+    print(f"dict().items: {item}")
+
+for item in sample_dict.keys():
+    print(f"dict().keys: {item}")
+
+for item in sample_dict.values():
+    print(f"dict().values: {item}")
