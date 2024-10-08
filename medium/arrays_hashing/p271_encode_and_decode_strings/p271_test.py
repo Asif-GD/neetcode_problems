05ca2ad0b83dd72c2fdd427bcd7ba5fcd6c7db87
Asif-GD <asif.coach@gmail.com>
2024-10-08 12:32:54 +0530

    - test string slicing

---
+++ medium/arrays_hashing/p271_encode_and_decode_strings/p271_test.py
@@ -0,0 +1,6 @@
+sample_string = "012345"
+sub_string_1 = sample_string[:]
+sub_string_2 = sample_string[:3]
+
+print(f"sample_string[:]: {sub_string_1}")
+print(f"sample_string[:3]: {sub_string_2}")
